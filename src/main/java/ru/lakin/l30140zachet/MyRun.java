ad99ddac5fd2fd875f635b1869216b7afa4261ed
Nikodim <artggl@bk.ru>
2022-06-26 00:40:56 +0300

    Лабы 13, 14 + Зачёт

--- src/main/java/ru/lakin/l30140zachet/MyRun.java
+++ src/main/java/ru/lakin/l30140zachet/MyRun.java
@@ -32,7 +32,7 @@ public class MyRun extends Application {
         if (clrRed.isSelected()) allSelected += "Аз ";
         if (clrGreen.isSelected()) allSelected += "Буки ";
         if (clrBlue.isSelected()) allSelected += "Веди";
-        if (allSelected.equals("Выбрано: ")) selectedColors.setText("ничо :(");
+        if (allSelected.equals("Выбрано: ")) selectedColors.setText(allSelected + "ничо :(");
         else selectedColors.setText(allSelected);
     }
 
@@ -63,6 +63,11 @@ public class MyRun extends Application {
 
             // --- Пустая строка
             Text txtBlank = new Text(" ");
+            Text txt1Blank = new Text(" ");
+
+            // --- СПРАВОЧНИК ПО ЧЕК-БОКСАМ
+            Text txt1 = new Text("СПРАВОЧНИК ПО ЧЕК-БОКСАМ");
+            txt1.setStyle("-fx-font-size: 13pt; -fx-font-weight: bold;");
 
             // --- Чек-бокс
             Label selectedColors = new Label("Выбрано: Аз");
@@ -78,6 +83,10 @@ public class MyRun extends Application {
             clrGreen.setOnAction(event -> showColors(clrRed, clrGreen, clrBlue, selectedColors));
             clrBlue.setOnAction(event -> showColors(clrRed, clrGreen, clrBlue, selectedColors));
 
+            // --- СПРАВОЧНИК ПО СТРОКЕ ВВОДА
+            Text txt2 = new Text("СПРАВОЧНИК ПО СТРОКЕ ВВОДА");
+            txt2.setStyle("-fx-font-weight: bold;");
+
             // --- Поле текстового ввода с кнопочкой
             String patsakSays = "   Пацак сказал: ";
             Label lblText = new Label(patsakSays);
@@ -86,6 +95,10 @@ public class MyRun extends Application {
             Button btnTextIn = new Button("Ку!");
             // отправка текста куда-нить
             btnTextIn.setOnAction(event -> lblText.setText(patsakSays + textField.getText()));
+
+            // СПРАВОЧНИК ПО ТРЕУГОЛЬНИКАМ И КРУГАМ
+            Text txt3 = new Text("СПРАВОЧНИК ПО ТРЕУГОЛЬНИКАМ И КРУГАМ");
+            txt3.setStyle("-fx-font-weight: bold;");
 
             // --- Круг
             circle = new Circle(100, Color.RED);
@@ -111,58 +124,86 @@ public class MyRun extends Application {
             btnPryg.setOnAction(pryg);
             btnKysh.setOnAction(kysh);
 
+            // СПРАВОЧНИК ПО НАРОДУ
+            Text txt4 = new Text("СПРАВОЧНИК ПО НАРОДУ");
+            txt4.setStyle("-fx-font-weight: bold;");
+
             // --- Текст
             Text txt = new Text();
-            txt.setStyle("-fx-font-size: 11pt; -fx-font-family: \"Segoe UI Semibold\";" +
-                    "-fx-text-fill: navy; -fx-opacity: 1;");
+            txt.setStyle("-fx-font-family: \"Segoe UI Semibold\";" +
+                         "-fx-font-size: 11pt; -fx-text-fill: navy; -fx-opacity: 1;");
             txt.setText("Народ предпочитает синие кнопки!\n ");
 
 // -------- Примитивы - в горизонтальные панели!
 
-            // --- Панель 0 "горизонтальная линия" для примитивов (чек-боксы)
-            HBox bxHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (ЗАГОЛОВОК)
+            HBox txt1HBox = new HBox();
+            // --- добавляем примитивы в панель для примитивов
+            txt1HBox.setAlignment(Pos.CENTER);
+            txt1HBox.getChildren().add(txt1);
+
+            // --- Панель "горизонтальная линия" для примитивов (чек-боксы)
+            HBox bxHBox = new HBox(17);
             // --- добавляем примитивы в панель для примитивов
             bxHBox.setAlignment(Pos.CENTER);
             bxHBox.getChildren().add(clrRed);
             bxHBox.getChildren().add(clrGreen);
             bxHBox.getChildren().add(clrBlue);
 
-            // --- Панель 1 "горизонтальная линия" для примитивов (результат чек-боксов)
-            HBox bxrzHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (результат чек-боксов)
+            HBox bxrzHBox = new HBox();
             // --- добавляем примитивы в панель для примитивов
             bxrzHBox.setAlignment(Pos.CENTER);
             bxrzHBox.getChildren().add(selectedColors);
             bxrzHBox.getChildren().add(lblText);
 
-            // --- Панель 2 "горизонтальная линия" для примитивов (ввод в текстовую строку и кнопочка)
-            HBox txtInHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (ЗАГОЛОВОК)
+            HBox txt2HBox = new HBox();
+            // --- добавляем примитивы в панель для примитивов
+            txt2HBox.setAlignment(Pos.CENTER);
+            txt2HBox.getChildren().add(txt2);
+
+            // --- Панель "горизонтальная линия" для примитивов (ввод в текстовую строку и кнопочка)
+            HBox txtInHBox = new HBox(7);
             // добавляем примитивы в панель для примитивов
             txtInHBox.setAlignment(Pos.CENTER);
             txtInHBox.getChildren().add(textField);
             txtInHBox.getChildren().add(btnTextIn);
 
-            // --- Панель 3 "горизонтальная линия" для примитивов (результат ввода в текстовую строку)
-            HBox txtInrzHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (результат ввода в текстовую строку)
+            HBox txtInrzHBox = new HBox();
             // добавляем примитивы в панель для примитивов
             txtInrzHBox.setAlignment(Pos.CENTER);
             txtInrzHBox.getChildren().add(lblText);
 
-            // --- Панель 4 "горизонтальная линия" для фигур
-            shpHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (ЗАГОЛОВОК)
+            HBox txt3HBox = new HBox();
+            // --- добавляем примитивы в панель для примитивов
+            txt3HBox.setAlignment(Pos.CENTER);
+            txt3HBox.getChildren().add(txt3);
+
+            // --- Панель "горизонтальная линия" для фигур
+            shpHBox = new HBox();
             // добавляем примитивы в панель для примитивов
             shpHBox.setAlignment(Pos.CENTER);
             shpHBox.getChildren().add(circle);
             shpHBox.getChildren().add(triangle);
 
             // --- Панель 5 "горизонтальная линия" для кнопок
-            HBox btnHBox = new HBox(30);
+            HBox btnHBox = new HBox(17);
             // добавляем примитивы в панель для примитивов
             btnHBox.setAlignment(Pos.CENTER);
             btnHBox.getChildren().add(btnPryg);
             btnHBox.getChildren().add(btnKysh);
 
-            // --- Панель 6 "горизонтальная линия" для текста
-            HBox txtHBox = new HBox(30);
+            // --- Панель "горизонтальная линия" для примитивов (ЗАГОЛОВОК)
+            HBox txt4HBox = new HBox();
+            // --- добавляем примитивы в панель для примитивов
+            txt4HBox.setAlignment(Pos.CENTER);
+            txt4HBox.getChildren().add(txt4);
+
+            // --- Панель "горизонтальная линия" для текста
+            HBox txtHBox = new HBox();
             // добавляем примитивы в панель для примитивов
             txtHBox.setAlignment(Pos.CENTER);
             txtHBox.getChildren().add(txt);
@@ -170,16 +211,23 @@ public class MyRun extends Application {
 // -------- Горизонтальные панели - в вертикальные панели!
 
             // Панель "Вертикальная линия" - контейнер для горизонтальных панелей
-            VBox cntVBox = new VBox(30);
+            VBox cntVBox = new VBox(17);
             // добавляем примитивы и панели в панель для примитивов
-
             cntVBox.getChildren().add(txtBlank);
+            cntVBox.getChildren().add(txt1HBox);
             cntVBox.getChildren().add(bxHBox);
             cntVBox.getChildren().add(bxrzHBox);
+
+            cntVBox.getChildren().add(txt2HBox);
             cntVBox.getChildren().add(txtInHBox);
             cntVBox.getChildren().add(txtInrzHBox);
+
+            cntVBox.getChildren().add(txt3HBox);
             cntVBox.getChildren().add(shpHBox);
             cntVBox.getChildren().add(btnHBox);
+
+            cntVBox.getChildren().add(txt1Blank);
+            cntVBox.getChildren().add(txt4HBox);
             cntVBox.getChildren().add(txtHBox);
 
 // -------- Ну и всё!
